79322bf6bb65f772cd38914bd82875ffd375b8cc
abe hernandez <dbaknack@gmail.com>
2022-03-10 08:32:29 -0700

    working on understanding of how LDAP queries are structured in a way to faciliate automation

---
+++ Display-UDFDefaultName.ps1
@@ -0,0 +1,26 @@
+<#
+ when using pw to create a user account, i can't control the format of the display name.
+ the GUI tool defaults to displaying the name as first name followed by last name.
+
+this names browsing more difficult since first names tend not to as selective as last names.
+
+task: modify the way display names are created when using the GUI tools. change default behavior
+#>
+
+$rootDSE    = [ADSI]"LDAP://RootDSE"
+$dispspec   = [ADSI] ("LDAP://cn=User-Display,cn=409,cn=DisplaySpecifiers," + $rootDSE.ConfigurationNamingContext)
+$dispspec.CreateDialog = "%<sn> %<givenName>"
+$dispspec.SetInfo()
+
+$disp = [ADSI]("LDAP://,cn=409,cn=DisplaySpecifiers," + rootDSE.ConfigurationNamingContext)
+$disp.children
+
+Get-ADObject  `
+        -Identity "cn=User-Display,cn=409,cn=DisplaySpecifiers, `
+        $((Get-ADRootDSE).configurationNamingContext)" `
+        -Properties CreateDialog |
+        Format-List *
+        Set-ADObject `
+        -Identity "cn=User-Display,cn=409,cn=DisplaySpecifiers,`
+        $((Get-ADRootDSE).configurationNamingContext)"  `
+        -Replace @{CreateDialog = "%<sn> %<givenName>"}
