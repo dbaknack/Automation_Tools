b2a6d13e125099e3368acbf36fb9810da1898c2e
Abe H <dbaknack@gmail.com>
2022-03-10 01:07:27 -0700

    Add files via upload

---
+++ Format-UDFColorFile.ps1
@@ -0,0 +1,52 @@
+function Format-Colorfile {
+    <# 1
+    .SYNOPSIS
+        Creates a colored listing of a folders files
+    .DESCRIPTION
+        Uses [System.Console]::ForegroundColor to control
+        the color used to write out the file name.
+        This is based on a PowerShell filter from
+        the very first Monad book.
+    .NOTES
+    .LINK
+    .EXAMPLE
+        "Get-ChildItem c:\windows | Format-Colorfile"
+    .INPUTS
+        output of Get-ChildItem
+    .OUTPUTS
+        None
+    .PARAMETER file
+    #>
+    #[CmdletBinding(SupportsShouldProcess=$True)]
+    param (
+        [Parameter(ValueFromPipeline=$true)] $file
+    )
+    begin {
+        $colors =   @{
+            ps1         =   "Cyan";
+            exe         =   "Green";
+            cmd         =   "Green";
+            directory   =   "Yellow"
+    }
+        $defaultColor   =   "White"
+    }
+    process {
+        # condition where nothing is handled
+        if ($file.Extension -ne "") {
+            $ext = $file.Extension.Substring(1)
+            Write-Output $ext
+        }
+        # condtion where extension is something
+        if ($file.Mode.StartsWith("d")) {
+            $ext = "directory" 
+        }
+        if ($colors.ContainsKey($ext)) {
+            [System.Console]::ForegroundColor = $colors[$ext] 
+        }
+        $file
+        [System.Console]::ForegroundColor = $defaultColor
+    }
+    end {
+        [System.Console]::ForegroundColor = $defaultColor
+    }
+}
